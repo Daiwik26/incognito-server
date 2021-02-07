5af5c81fd3a71af476f9bfda7dbfe4c0f6530f8d
Daiwik Khandelwal <daiwikkhandelwal2006@gmail.com>
2021-02-07 18:48:21 +0530

    Update server.js server-running

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
 const express = require('express');
 
+// gets dotenv module to use env vars.
+require('dotenv').config()
+
 const server = express();
+
+// starts server on specified env var.
+server.listen(process.env.PORT, (err) => {
+
+	// logs error if one has occurred.
+	console.log(err ? `Fatal server start-up error has occurred: ${err}` :
+				`Server is listening on port ${process.env.PORT}...`);
+});
